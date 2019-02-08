016da0af42e5a0cd567db4705d7deb98b42a8ece
IsuruGodakanda <isuru.godakanda@my.sliit.lk>
2019-02-08 12:01:40 +0530

    Adding Validations and Loggings

--- src/idea/idea.dto.ts
+++ src/idea/idea.dto.ts
@@ -1,5 +1,10 @@
+import { IsString } from 'class-validator';
+
 // Detail the type of data that's been pass through the controller.
-export interface IdeaDTO {
+export class IdeaDTO {
+  @IsString()
   idea: string;
+
+  @IsString()
   description: string;
 }

191b42f99d86e177f9a44ab186002d7cceffc70e
IsuruGodakanda <isuru.godakanda@my.sliit.lk>
2019-02-13 10:15:57 +0530

    Added Join Table

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -4,7 +4,9 @@ import {
   CreateDateColumn, 
   Column, 
   BeforeInsert, 
-  OneToMany
+  OneToMany,
+  ManyToMany,
+  JoinTable
 } from "typeorm";
 import * as bcrypt from 'bcryptjs';
 import * as jwt from 'jsonwebtoken';
@@ -29,6 +31,10 @@ export class UserEntity {
   @OneToMany(type => IdeaEntity, idea => idea.author)
   ideas: IdeaEntity[];
 
+  @ManyToMany(type => IdeaEntity, {cascade: true})
+  @JoinTable()
+  bookmarks: IdeaEntity[];
+
   @BeforeInsert()
   async hashPassword() {
     this.password = await bcrypt.hash(this.password, 10);
@@ -42,6 +48,9 @@ export class UserEntity {
     }
     if(this.ideas) {
       responseObject.ideas = this.ideas;
+    }
+    if(this.bookmarks) {
+      responseObject.bookmarks = this.bookmarks;
     }
     return responseObject;
   }

766a4f27ba82a0a81176a74ba08bb4daeb1d6baa
IsuruGodakanda <isuru.godakanda@my.sliit.lk>
2019-02-13 19:36:15 +0530

    Added Basic GraphQL queries

--- src/idea/idea.module.ts
+++ src/idea/idea.module.ts
@@ -5,12 +5,15 @@ import { IdeaEntity } from './idea.entity';
 import { IdeaController } from './idea.controller';
 import { IdeaService } from './idea.service';
 import { UserEntity } from 'src/user/user.entity';
+import { IdeaResolver } from './idea.resolver';
+import { CommentEntity } from 'src/comment/comment.entity';
+import { CommentService } from 'src/comment/comment.service';
 
 
 @Module({
   // Connect IdeaEntity to database
-  imports: [TypeOrmModule.forFeature([IdeaEntity, UserEntity])],
+  imports: [TypeOrmModule.forFeature([IdeaEntity, UserEntity, CommentEntity])],
   controllers: [IdeaController],
-  providers: [IdeaService]
+  providers: [IdeaService, IdeaResolver, CommentService]
 })
 export class IdeaModule {}
